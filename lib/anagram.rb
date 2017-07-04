36b44287560fa989205812756358d48fb82d2048
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-07-04 18:18:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(anagrams)
+    results = []
+    anagrams.each do |word|
+      if word.split("").sort == self.name.split("").sort
+        results << word
+      end
+    end
+    results
+  end
+end
